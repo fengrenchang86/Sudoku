eb01fbe64045f6ad453cb8c60930dc43ce693e94
dfeng <fengrenchang86@vip.qq.com>
2016-03-13 11:01:43 +0800

    Adding TestPanel

--- src/com/frc/sudoku/Panel/impl/TestPanel.java
+++ src/com/frc/sudoku/Panel/impl/TestPanel.java
@@ -1,6 +1,7 @@
 package com.frc.sudoku.Panel.impl;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -11,6 +12,7 @@ import java.awt.event.MouseListener;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
@@ -28,65 +30,15 @@ public class TestPanel extends JPanel implements IWFPanel, ActionListener {
 		JPanel mainPanel = new JPanel();
 		mainPanel.setBorder(BorderFactory.createMatteBorder(1,1,1,1, Color.BLACK));
 		
-		final JTextArea txtArea = new JTextArea("abc");
-		JLabel label = new JLabel("<html>1 2 3</br></html>");
-		label.addMouseListener(new MouseListener() {
-
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				JLabel label = (JLabel)e.getSource();
-				String str = label.getText();
-				System.out.println(label);
-				txtArea.setText(str);
-			}
-
-			@Override
-			public void mouseEntered(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void mouseExited(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void mousePressed(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void mouseReleased(MouseEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-		});
-		this.addKeyListener(new KeyListener(){
-
-			@Override
-			public void keyPressed(KeyEvent e) {
-				char ch = e.getKeyChar();
-				int code = e.getKeyCode();
-				System.out.println(String.format("keyChar:%c, code:%d", ch, code));
-			}
-
-			@Override
-			public void keyReleased(KeyEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void keyTyped(KeyEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}});
-		mainPanel.add(label);
-		mainPanel.add(txtArea);
+		JTextArea txtArea = new JTextArea("111222\n33123123213123k21ll3jlk______3", 3, 30);
+		txtArea.setLineWrap(true);
+		
+		JScrollPane areaScrollPane = new JScrollPane(txtArea);
+		areaScrollPane.setVerticalScrollBarPolicy(
+                JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		areaScrollPane.setPreferredSize(new Dimension(250, 250));
+		
+		mainPanel.add(areaScrollPane);
 		
 		this.add(mainPanel);
 	}
